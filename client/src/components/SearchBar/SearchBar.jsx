e54b2059baebb6da4c628255e49a6c688c73f6de
Gonza <gonzaloezequielavila@gmail.com>
2024-03-27 15:34:08 -0300

    working on searchBar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,23 @@
 import axios from 'axios';
 import { useState } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {search}
+import {search} from '../../redux/actions/actions'
+
+
+const SearchBar = () => {
+
+  const dispatch = useDispatch();
+
+  const {countries} = useSelector (state=>{
+    return state;
+  })
+
+  return (
+    <div>
+
+
+    </div>
+  )
+};
+
+export default SearchBar;
